6e561cfa51bea03393089786f802abc46459e14a
noobHasan <aceHasan7@gmail.com>
2025-07-08 00:28:11 +0600

    Day5 : JumpAnimation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,22 @@ public class PlayerController : MonoBehaviour
 {
     public Animator playerAnimator;
     public float moveSpeed = 5f;
+    private float jumpForce = 6f;
 
+    private Rigidbody rb;
+    [SerializeField] private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = new Vector3(moveX, transform.position.y, moveZ);
+        Vector3 moveDirection = new Vector3(moveX, 0, moveZ);
 
         if (moveDirection.magnitude > 0)
         {
@@ -25,5 +33,29 @@ public class PlayerController : MonoBehaviour
             playerAnimator.SetBool("isRunning", false);
         }
 
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnimator.SetBool("isJumping", true);
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            playerAnimator.SetBool("isJumping", false);
+
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
