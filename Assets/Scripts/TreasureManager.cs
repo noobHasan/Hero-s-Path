13ea70a3b13772d6d49e3fab7150fdfe9d0a9581
noobHasan <95558492+noobHasan@users.noreply.github.com>
2025-12-16 22:04:55 +0600

    Day 13 : RandomSpawnPoints

--- Assets/Scripts/TreasureManager.cs
+++ Assets/Scripts/TreasureManager.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TreasureManager : MonoBehaviour
 {
     public static TreasureManager instance;
+
+    public GameObject treasurePrefab;
+    public List<Transform> spawnPoints = new List<Transform>();
     public int totalTreasure;
     private int collectedTreasures;
     private void Awake()
     {
         instance = this;
+    }
+    private void Start()
+    {
+        SpawnTreasures();
     }
 
     public void CollectTreasure()
@@ -15,5 +23,18 @@ public class TreasureManager : MonoBehaviour
         collectedTreasures++;
 
         Debug.Log("Treasure collected :" + collectedTreasures + "/" + totalTreasure);
+    }
+
+    void SpawnTreasures()
+    {
+        List<Transform> availablePoints = new List<Transform>(spawnPoints);
+
+        for(int i = 0; i<totalTreasure; i++)
+        {
+            int randomIndex = Random.Range(0,availablePoints.Count);
+            Transform spawnPoint = availablePoints[randomIndex];
+            Instantiate(treasurePrefab,spawnPoint.position,Quaternion.identity);
+            availablePoints.RemoveAt(randomIndex);
+        }
     }
 }
