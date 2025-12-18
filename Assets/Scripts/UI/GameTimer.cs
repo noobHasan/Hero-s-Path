368d003bbc48f843c3dd13ab6d059b1ed39ea4b4
noobHasan <95558492+noobHasan@users.noreply.github.com>
2025-12-19 01:24:10 +0600

    Day 15 - TimerSystem

---
+++ Assets/Scripts/UI/GameTimer.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using TMPro;
+
+public class GameTimer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float startTime = 60f;
+
+    private float currentTime;
+    private bool timerStarted;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentTime = startTime;
+        UpdateTimerUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!timerStarted && IsPlayerInput())
+        {
+            timerStarted = true;
+        }
+
+        if (timerStarted)
+        {
+            currentTime -= Time.deltaTime;
+            currentTime = Mathf.Clamp(currentTime,0f,startTime);
+
+            UpdateTimerUI();
+
+            if(currentTime <= 0)
+            {
+                TimerEnded();
+            }
+        }
+    }
+
+    public void UpdateTimerUI()
+    {
+        timerText.text = "Timer: " + Mathf.CeilToInt(currentTime);
+    }
+
+    bool IsPlayerInput()
+    {
+        return Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0 || Input.GetButtonDown("Jump");
+    }
+
+    public void TimerEnded()
+    {
+        timerStarted = false;
+        Debug.Log("Time Up!");
+    }
+}
